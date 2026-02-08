4ae97cd41a23f88444357527c4471aa2a6b1ba86
ManojGSS144 <manojk@torus.tech>
2026-02-09 01:51:33 +0530

    loader included

--- app/billing/page.tsx
+++ app/billing/page.tsx
@@ -3,7 +3,13 @@ import BillingPage from "./_components/BillingClient";
 
 export default function Page() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense
+      fallback={
+        <div className="flex justify-center items-center h-screen">
+          <img src="/asset.svg" alt="Loading..." className="w-16 h-16" />
+        </div>
+      }
+    >
       <BillingPage />
     </Suspense>
   );
