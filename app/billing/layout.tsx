1a10f1597f00a9a51e792090a34b7098f527ced4
ManojGSS144 <manojk@torus.tech>
2026-02-09 02:03:29 +0530

    home icon implemented

--- app/billing/layout.tsx
+++ app/billing/layout.tsx
@@ -1,16 +1,35 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/theme-switch";
+import { HomeIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function BillingLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
-    return (
-        <div className="relative w-full h-full ">
-            <div className="fixed top-5 right-5 " >
-                <ModeToggle />
-            </div>
-            {children}
-        </div>
-    );
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const router = useRouter();
+
+  return (
+    <div className="relative w-full min-h-screen">
+
+      <div className="fixed top-5 left-5 z-50">
+        <Button
+          variant="outline"
+          size="icon"
+          onClick={() => router.push("/")} 
+        >
+          <HomeIcon className="w-5 h-5" />
+        </Button>
+      </div>
+
+      <div className="fixed top-5 right-5 z-50">
+        <ModeToggle />
+      </div>
+
+      {children}
+    </div>
+  );
 }
